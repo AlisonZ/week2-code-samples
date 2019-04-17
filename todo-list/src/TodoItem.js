893fc766baf1f47d8b934e5c7845117e19ac6ca8
bhague1281 <bhague1281@gmail.com>
2019-04-16 22:34:11 -0700

    todolist

---
+++ todo-list/src/TodoItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class TodoItem extends React.Component {
+    static propTypes = {
+        todo: PropTypes.string
+    }
+
+    render() {
+        return <li>{this.props.todo}</li>;
+    }
+}
+
+// TodoItem.propTypes = {
+//     todo: PropTypes.string
+// }
